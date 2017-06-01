57c21650ba4d36458375a9bf96548e01e4189d1e
Terry Ye <authinkingwise@gmail.com>
2017-06-01 23:25:50 +1000

    Add a new file

---
+++ src/test/java/com/interval/teamglue/TestServlet.java
@@ -0,0 +1,12 @@
+package com.interval.teamglue;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class TestServlet extends TestCase {
+	@Test
+	public void test1() {
+		System.out.println("This is a test");
+	}
+}
